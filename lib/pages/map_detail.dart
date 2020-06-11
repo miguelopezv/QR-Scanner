dcc1e25de62b2cda1484104087f30fc35490315e
miguelopezv <miguelopezv@gmail.com>
2020-06-11 12:45:03 -0500

    update API and markpoint

--- lib/pages/map_detail.dart
+++ lib/pages/map_detail.dart
@@ -28,18 +28,37 @@ class MapDetailPage extends StatelessWidget {
         center: scan.getLatLng(),
         zoom: 10,
       ),
-      layers: [_createMap()],
+      layers: [
+        _createMap(),
+        _createMarkpoint(scan),
+      ],
     );
   }
 
   _createMap() {
     return TileLayerOptions(
-        urlTemplate: 'https://api.tiles.mapbox.com/v4/'
-            '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+        urlTemplate:
+            'https://api.mapbox.com/v4/{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
         additionalOptions: {
           'accessToken':
-              'pk.eyJ1IjoibWlndWVsb3BlenYiLCJhIjoiY2tiYXp6aWdtMHRjbTJ3bzB0cm8wNXhpdiJ9.L8-ONFQLttNpkqg3dhQwnw',
+              'pk.eyJ1Ijoiam9yZ2VncmVnb3J5IiwiYSI6ImNrODk5aXE5cjA0c2wzZ3BjcTA0NGs3YjcifQ.H9LcQyP_-G9sxhaT5YbVow',
           'id': 'mapbox.streets'
         });
+  }
+
+  _createMarkpoint(ScanModel scan) {
+    return MarkerLayerOptions(markers: <Marker>[
+      Marker(
+          width: 120,
+          height: 120,
+          point: scan.getLatLng(),
+          builder: (context) => Container(
+                child: Icon(
+                  Icons.location_on,
+                  size: 45,
+                  color: Theme.of(context).primaryColor,
+                ),
+              )),
+    ]);
   }
 }
