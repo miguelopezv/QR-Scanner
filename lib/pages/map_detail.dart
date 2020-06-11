89e03b60da2f27970f6a16309b848e0395b94913
miguelopezv <miguelopezv@gmail.com>
2020-06-11 11:48:19 -0500

    render initial map

--- lib/pages/map_detail.dart
+++ lib/pages/map_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:qr_scanner/providers/db_provider.dart';
+import 'package:flutter_map/plugin_api.dart';
+
+import '../models/scan_model.dart';
 
 class MapDetailPage extends StatelessWidget {
   @override
@@ -16,9 +18,28 @@ class MapDetailPage extends StatelessWidget {
           )
         ],
       ),
-      body: Center(
-        child: Text(_scan.value),
-      ),
+      body: _renderMap(_scan),
     );
+  }
+
+  Widget _renderMap(ScanModel scan) {
+    return FlutterMap(
+      options: MapOptions(
+        center: scan.getLatLng(),
+        zoom: 10,
+      ),
+      layers: [_createMap()],
+    );
+  }
+
+  _createMap() {
+    return TileLayerOptions(
+        urlTemplate: 'https://api.tiles.mapbox.com/v4/'
+            '{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}',
+        additionalOptions: {
+          'accessToken':
+              'pk.eyJ1IjoibWlndWVsb3BlenYiLCJhIjoiY2tiYXp6aWdtMHRjbTJ3bzB0cm8wNXhpdiJ9.L8-ONFQLttNpkqg3dhQwnw',
+          'id': 'mapbox.streets'
+        });
   }
 }
