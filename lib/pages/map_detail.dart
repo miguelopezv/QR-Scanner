cdf11bb9978378366fef7bde8a828859406180a7
miguelopezv <miguelopezv@gmail.com>
2020-06-11 13:15:40 -0500

    center marker on button click

--- lib/pages/map_detail.dart
+++ lib/pages/map_detail.dart
@@ -4,9 +4,12 @@ import 'package:flutter_map/plugin_api.dart';
 import '../models/scan_model.dart';
 
 class MapDetailPage extends StatelessWidget {
+  final MapController mapCtrl = new MapController();
+
   @override
   Widget build(BuildContext context) {
     final ScanModel _scan = ModalRoute.of(context).settings.arguments;
+    final double _zoom = 12;
 
     return Scaffold(
       appBar: AppBar(
@@ -14,19 +17,22 @@ class MapDetailPage extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.my_location),
-            onPressed: () {},
+            onPressed: () {
+              mapCtrl.move(_scan.getLatLng(), _zoom);
+            },
           )
         ],
       ),
-      body: _renderMap(_scan),
+      body: _renderMap(_scan, _zoom),
     );
   }
 
-  Widget _renderMap(ScanModel scan) {
+  Widget _renderMap(ScanModel scan, double zoom) {
     return FlutterMap(
+      mapController: mapCtrl,
       options: MapOptions(
         center: scan.getLatLng(),
-        zoom: 10,
+        zoom: zoom,
       ),
       layers: [
         _createMap(),
