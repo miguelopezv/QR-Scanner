6fed45c56321b6388b35339e76c4a21c6ca0c04e
miguelopezv <miguelopezv@gmail.com>
2020-06-11 11:27:19 -0500

    update util to navigate to map detail

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 import '../models/scan_model.dart';
 
-openScan(ScanModel scan) async {
+openScan(BuildContext context, ScanModel scan) async {
   if (scan.type == 'http') {
     if (await canLaunch(scan.value)) {
       await launch(scan.value);
     } else {
       throw 'Could not launch ${scan.value}';
     }
-  } else {}
+  } else {
+    Navigator.pushNamed(context, 'map', arguments: scan);
+  }
 }
