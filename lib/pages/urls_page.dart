9b86b990a1589d9af7f7efd13dba6df3af20b456
miguelopezv <miguelopezv@gmail.com>
2020-06-11 10:55:22 -0500

    connect view with bloc

--- lib/pages/urls_page.dart
+++ lib/pages/urls_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:qr_scanner/models/scan_model.dart';
+
+import '../bloc/scans_bloc.dart';
 
 class UrlsPage extends StatelessWidget {
-  const UrlsPage({Key key}) : super(key: key);
+  final _scansBloc = new ScansBloc();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Urls'),
-      ),
+    return StreamBuilder(
+      stream: _scansBloc.scansStream,
+      builder: (BuildContext context, AsyncSnapshot<List<ScanModel>> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.data.length == 0) {
+          return Center(
+            child: Text('No data'),
+          );
+        }
+
+        return ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, i) => Dismissible(
+                  key: UniqueKey(),
+                  background: Container(color: Colors.red),
+                  onDismissed: (direction) =>
+                      _scansBloc.deleteScan(snapshot.data[i].id),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.cloud_queue,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                    title: Text(snapshot.data[i].value),
+                    subtitle: Text('ID: ${snapshot.data[i].id}'),
+                    trailing:
+                        Icon(Icons.keyboard_arrow_right, color: Colors.grey),
+                  ),
+                ));
+      },
     );
   }
 }
